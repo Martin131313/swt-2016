171a3ecaab82565937eb6b336a0c77ae49576669
Martin131313 <moc@semosedu.local>
2017-02-08 12:43:17 +0100

    vtoroto ke da e

---
+++ c1HTTP/cc1.php
@@ -0,0 +1,35 @@
+<?php
+
+$f1 = 420;
+$f2 = 69;
+$f3 = 666;
+
+// if($f1 > $f2 && $f3 < $f1){
+// 	echo "Koj e toj pokemon";
+
+// } elseif($f2 > $f1 && $f3 < $f2){
+// 	echo "Vtoroto ke da e";
+
+// } else {
+// 	echo "Mozda e tretoto";
+// };
+
+
+if($f1 > $f2){
+ if($f1 > $f3){
+
+ }
+ else {
+ 	echo "tochak";
+ }
+}
+else {
+ echo "vaka";
+ }
+
+
+
+
+
+
+?>
